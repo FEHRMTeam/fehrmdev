cd5d3114f200ac98d281ef038b5ef8997023a4f2
Rob Felder <felder_robert@bah.com>
2021-06-14 16:56:38 -0400

    Updated colors, footer logo, and footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from '../images/footer-logo.png';
+import logo from '../images/fehrmlogo.png';
+import { Link } from 'gatsby';
 
 const Footer = () => (
   <footer className="usa-footer site-footer" role="contentinfo">
@@ -16,16 +17,16 @@ const Footer = () => (
           <div className="tablet:grid-col"></div>
           <div className="tablet:grid-col">
             <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
+              <li><Link to="/about-fehrm">About Us</Link></li>
+              <li><Link to="/contact-us">Contact Us</Link></li>
+              <li><Link to="/faq"> Frequently Asked Questions</Link></li>
             </ul>
           </div>
           <div className="tablet:grid-col">
             <ul className="add-list-reset">
-              <li>Footer link</li>
-              <li>Footer link</li>
-              <li>Footer link</li>
+              <li><Link to="https://www.linkedin.com/company/fehrm/">FEHRM LinkedIn Page</Link></li>
+              <li><Link to="https://www.ehrm.va.gov/">Office of Electronic health Record Modernization</Link></li>
+              <li><Link to="https://www.health.mil/About-MHS/OASDHA/Defense-Health-Agency/Defense-Healthcare-Management-Systems">Defense Healthcare Management Systems</Link></li>
             </ul>
           </div>
         </div>
