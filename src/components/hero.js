b7f26420984d562b59a6d4a668a929600a06805f
Rob Felder <felder_robert@bah.com>
2021-07-21 15:28:34 -0400

    Integrating react-slick and slick-carousel

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Slider from "react-slick";
 
 /*
   This will be displayed on the homepage. Ideally, you want to highlight key goals of the website
 */
 
 // const Hero = ({ heroCallout }) => (
-const Hero = () => (
+const Hero = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+  };
+
+  return (
   <section className="usa-hero">
-    <div className="grid-container">
+    {/* <div className="grid-container"> */}
+      <Slider {...settings}>
+        <div className="img-contain" style="color:black">
+        {/* style="background-image: url(/images/home-hero.png);"> */}
+          {/* <h3>test</h3> */}
+          <img src="/images/home-hero.png" />
+        </div>
+        <div className="img-contain" style="color:black">
+        {/* style="background-image: url(/images/home-hero.png);"> */}
+          {/* <h3>test2</h3> */}
+          <img src="/images/home-hero.png" />
+        </div>
+      </Slider>
       {/* <div className="usa-hero__callout">
         <h2 className="usa-hero__heading"
             dangerouslySetInnerHTML={{ __html: heroCallout.calloutHeaderText }}>
@@ -18,8 +41,8 @@ const Hero = () => (
           {heroCallout.callToActionBtnText}
         </a>
       </div> */}
-    </div>
+    {/* </div> */}
   </section>
-);
+)};
 
 export default Hero;
