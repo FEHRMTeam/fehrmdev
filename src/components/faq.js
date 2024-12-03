d1f55674e7620610e451f7c183db846b2bd01a73
Jennifer Frazier <jfrazier@keymind.com>
2024-12-03 13:01:54 -0500

    Add unique identifier to each question/answer pair to facilitate accordion function, update each FAQ to have unique title

--- src/components/faq.js
+++ src/components/faq.js
@@ -4,16 +4,16 @@ import React from 'react';
   This FAQ will appear in FAQ pages
 */
 
-const Faq = ({ content, uid, expand }) => {
+const Faq = ({ content, expand }) => {
 
   return (
     <>
         <h3 className="usa-accordion__heading">
-            <button type="button" className="usa-accordion__button" aria-expanded={expand} aria-controls={uid}>
+            <button type="button" className="usa-accordion__button" aria-expanded={expand} aria-controls={content.questionid}>
                 {content.question}
             </button>
         </h3>
-        <div id={uid} className="usa-accordion__content" hidden={( expand === false ? "hidden" : "")}>
+        <div id={content.questionid} className="usa-accordion__content" hidden={( expand === false ? "hidden" : "")}>
             <p dangerouslySetInnerHTML={{ __html: content.answer}}></p>
         </div>
     </>
