3b210885f67b65d0bd6270ecb0273fefe2b326bd
Jennifer Frazier <jfrazier@keymind.com>
2024-12-04 11:25:37 -0500

    Add birthday logos to header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,19 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import useSiteMetadata from '../hooks/use-site-metadata';
 
 const Header = ({ children, siteTitle = `` }) => {
+
+  const { basePath } = useSiteMetadata();
+
   return (
   <header className="usa-header usa-header--extended" role="banner">
     <div className="usa-navbar">
       <div className="usa-logo" id="extended-logo">
         <em className="usa-logo__text">
           <Link to="/" title="Home" aria-label="Home">
-            {siteTitle}
+            <img src={`${basePath}/images/FEHRMHBDLogo_White.png`} alt={siteTitle} className="header-logo"/>
           </Link>
         </em>
       </div>
