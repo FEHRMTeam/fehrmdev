185b940c51b2a854f3dc9f1f69d3f4db4fa74e77
Jennifer Frazier <jfrazier@keymind.com>
2025-01-06 12:32:50 -0500

    Remove birthday logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,8 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import useSiteMetadata from '../hooks/use-site-metadata';
 
 const Header = ({ children, siteTitle = `` }) => {
-
-  const { basePath } = useSiteMetadata();
 
   return (
   <header className="usa-header usa-header--extended" role="banner">
@@ -13,7 +10,7 @@ const Header = ({ children, siteTitle = `` }) => {
       <div className="usa-logo" id="extended-logo">
         <em className="usa-logo__text">
           <Link to="/" title="Home" aria-label="Home">
-            <img src={`${basePath}/images/FEHRMHBDLogo_White.png`} alt={siteTitle} className="header-logo"/>
+            {siteTitle}
           </Link>
         </em>
       </div>
