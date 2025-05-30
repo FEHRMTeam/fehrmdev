7a8648d029ae972e91f083e6dd8712c88aa4273d
Jennifer Frazier <jfrazier@keymind.com>
2025-05-30 08:46:07 -0400

    Update 404 page to put content in <main> tag for accessibility

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,16 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="usa-layout-docs usa-section">
+      <div className="grid-container">
+        <div className="grid-row grid-gap">
+          <main>
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </main>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
